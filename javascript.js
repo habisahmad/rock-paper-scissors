581a335c3440c051a6e520934a22c8852c487d67
habisahmad <habis.ahmad123@gmail.com>
2022-06-05 18:56:52 -0400

    Function to play a round created with parameters, all conditions if it is a tie has been added

--- javascript.js
+++ javascript.js
@@ -4,4 +4,14 @@ function computerPlay(){
     return Math.floor(rand)
 }
 
-console.log(computerPlay())
+function round(playerSelection, computerSelection){
+    playerSelection = prompt("Please choose Rock, Paper, or Scissors")
+    computerSelection = computerPlay();
+    if(playerSelection == "Rock" && computerSelection == 1 || playerSelection == "Paper" && computerSelection == 2 || playerSelection == 3 && computerSelection == 3){
+        return "It is a tie!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(round(playerSelection, computerSelection));
