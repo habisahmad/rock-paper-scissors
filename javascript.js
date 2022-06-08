c1da56ff885f8d4154ecd2e2a931acc134436565
habisahmad <habis.ahmad123@gmail.com>
2022-06-07 22:07:31 -0400

    computerPlay function rewritten

--- javascript.js
+++ javascript.js
@@ -1,17 +1,14 @@
 function computerPlay(){
     let computerArray = ["Rock", "Paper", "Scissors"];
-    rand = Math.random(1) * computerArray.length + 1;
-    return Math.floor(rand)
+    const rand = Math.random(0) * computerArray.length;
+    let x = Math.floor(rand);
+    return computerArray.at(x);
 }
 
 function round(playerSelection, computerSelection){
-    playerSelection = prompt("Please choose Rock, Paper, or Scissors")
-    computerSelection = computerPlay();
-    if(playerSelection == "Rock" && computerSelection == 1 || playerSelection == "Paper" && computerSelection == 2 || playerSelection == 3 && computerSelection == 3){
-        return "It is a tie!"
-    }
+    let input = prompt("What is your answer?")
 }
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(round(playerSelection, computerSelection));
+round(playerSelection, computerSelection)
