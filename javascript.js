f5294ec7130fc3e0106b07901d604ebbce3c07a6
habisahmad <habis.ahmad123@gmail.com>
2022-06-28 20:52:01 -0400

    playerSelect function created to return the players click

--- javascript.js
+++ javascript.js
@@ -3,39 +3,22 @@ function computerPlay() {
     const x = Math.floor(Math.random() * (2 - 0 + 1) + 0)
     return array.at(x)
   }
-  function round(computerSelection){
-    let rock = "rock"
-    let scissors = "scissors"
-    let paper = "paper"
-    let playerSelection = prompt("What is your answer");
-    let playerSelect = playerSelection.toUpperCase();
-    let computerSelect = computerSelection.toUpperCase();
-    let compare = playerSelect.localeCompare(computerSelect);
-    win = 1;
-    loss = 0;
-    if(compare == 0){
-      return "It is a tie!"
-    }else if (playerSelect === rock.toUpperCase() && computerSelect === paper.toUpperCase()){ // All conditions in which the player loses
-      console.log("You have lost! Paper beats rock")
-      return loss
-    }else if(playerSelect === paper.toUpperCase() && computerSelect === scissors.toUpperCase()){
-      console.log("You have lost! Scissors beats paper")
-      return loss
-    }else if(playerSelect === scissors.toUpperCase() && computerSelect === rock.toUpperCase()){
-      console.log("You have lost! Rock beats Scisors") 
-      return loss
-    }else if (playerSelect === rock.toUpperCase() && computerSelect === scissors.toUpperCase() ){
-      console.log("You have Won! Rock beats Scissors") 
-      return win
-    }else if(playerSelect === paper.toUpperCase() && computerSelect === rock.toUpperCase()){
-      console.log("You have Won! Paper beats Rock")
-      return win
-    }else if(playerSelect === scissors.toUpperCase() && computerSelect === paper.toUpperCase()){
-      console.log("You have won! Scissors beats Paper!") 
-      return win
-    }
+
+function playerSelect(){
+  const rock = document.querySelector('.rock')
+  const scissors = document.querySelector('.scissors')
+  const paper = document.querySelector('.paper')
+  rock.onclick = () => {
+    return "Rock"
   }
-  
+  paper.onclick = () => {
+    return "Paper"
+  }
+  scissors.onclick = () => {
+    return "Scissors"
+  }
+}
+
   function game(win){
     let computerSelection = computerPlay()
     let ans = round(computerSelection);
@@ -49,9 +32,3 @@ function computerPlay() {
     }   
     return "Score: " + nt
   }
-  let computerSelection = computerPlay()
-for(let i = 0; i < 5; i++){
-    round(computerSelection)
-}
-console.log(game())
- 
