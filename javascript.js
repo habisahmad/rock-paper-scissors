f6aaa4187e031022bcc75db0d6a10fd91280105f
habisahmad <habis.ahmad123@gmail.com>
2022-06-30 18:47:23 -0400

    Function win() created to count how many rounds, and to end match after 5 wins

--- javascript.js
+++ javascript.js
@@ -24,38 +24,63 @@ scissors.addEventListener('click', () => {
 })
 let compScore = 0;
 let userScore = 0;
+
 function playRound(user, computer){
-  if(user == computer){
-    return "It is a tie!"
-  }else if(user == 'Rock'){
-    if(computer == 'Paper'){
-      compScore++
-      console.log(compScore, userScore)
-      return "You lose! Paper beats Rock"
-    }else if(computer == 'Scissors'){
-      userScore++
-      console.log(compScore, userScore)
-      return "You win! Rock beats Scissors"
+  if(userScore != 5 && compScore != 5){
+    if(user == computer){
+      win()
+      return "It is a tie!"
+    }else if(user == 'Rock'){
+      if(computer == 'Paper'){
+        compScore++
+        console.log(compScore, userScore)
+        win()
+        return "You lose! Paper beats Rock"
+      }else if(computer == 'Scissors'){
+        userScore++
+        console.log(compScore, userScore)
+        win()
+        return "You win! Rock beats Scissors"
+      }
+    }else if(user == 'Paper'){
+      if(computer == 'Rock'){
+        userScore++
+        console.log(compScore, userScore)
+        win()
+        return "You win! Paper beats Rock"
+      }else if(computer == 'Scissors'){
+        compScore++
+        console.log(compScore, userScore)
+        win()
+        return "You lose! Scissors beats Paper"
+      }
+    }else if(user == 'Scissors'){
+      if(computer == 'Rock'){
+        compScore++
+        console.log(compScore, userScore)
+        win()
+        return 'You lose! Rock beats Scissors'
+      }else if(computer == 'Paper'){
+        userScore++
+        console.log(compScore, userScore)
+        win()
+        return "You win! Scissors beats Paper"
+      }
     }
-  }else if(user == 'Paper'){
-    if(computer == 'Rock'){
-      userScore++
-      console.log(compScore, userScore)
-      return "You win! Paper beats Rock"
-    }else if(computer == 'Scissors'){
-      compScore++
-      console.log(compScore, userScore)
-      return "You lose! Scissors beats Paper"
+  }
+  
+  
+}
+let round = 0
+function win(){
+  if(userScore == 5 || compScore == 5){
+    if(compScore == 5){
+      console.log("Computer has won!")
+    }else if(userScore == 5){
+      console.log("User has won!")
     }
-  }else if(user == 'Scissors'){
-    if(computer == 'Rock'){
-      compScore++
-      console.log(compScore, userScore)
-      return 'You lose! Rock beats Scissors'
-    }else if(computer == 'Paper'){
-      userScore++
-      console.log(compScore, userScore)
-      return "You win! Scissors beats Paper"
-    }
+  }else{
+    round++
+    console.log("Rounds: ", round)
   }
 }
