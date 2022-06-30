31bf09e6f2b47cf3172d6feb7f1729a49989790c
habisahmad <habis.ahmad123@gmail.com>
2022-06-30 16:44:46 -0400

    playRound function added to compare player choice and computer choice. Event listeners added to define variables & call function

--- javascript.js
+++ javascript.js
@@ -1,34 +1,49 @@
-function computerPlay() {
-    array = ["Rock", "Paper", "Scissors"];
-    const x = Math.floor(Math.random() * (2 - 0 + 1) + 0)
-    return array.at(x)
-  }
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
 
-function playerSelect(){
-  const rock = document.querySelector('.rock')
-  const scissors = document.querySelector('.scissors')
-  const paper = document.querySelector('.paper')
-  rock.onclick = () => {
-    return "Rock"
-  }
-  paper.onclick = () => {
-    return "Paper"
-  }
-  scissors.onclick = () => {
-    return "Scissors"
+function computerSelect(){
+  options = ["Rock", "Paper", "Scissors"]
+  x = Math.floor(Math.random() * 3)
+  let computerChoice = options[x]
+  return computerChoice
+}
+function playerSelection(){
+  
+}
+rock.addEventListener('click', () => {
+  user = 'Rock'
+  console.log(playRound(user, computerSelect()))
+})
+paper.addEventListener('click', () => {
+  user = 'Paper'
+  console.log(playRound(user, computerSelect()))
+})
+scissors.addEventListener('click', () => {
+  user = 'Scissors'
+  console.log(playRound(user, computerSelect()))
+})
+
+function playRound(user, computer){
+  if(user == computer){
+    return "It is a tie!"
+  }else if(user == 'Rock'){
+    if(computer == 'Paper'){
+      return "You lose! Paper beats Rock"
+    }else if(computer == 'Scissors'){
+      return "You win! Rock beats Scissors"
+    }
+  }else if(user == 'Paper'){
+    if(computer == 'Rock'){
+      return "You win! Paper beats Rock"
+    }else if(computer == 'Scissors'){
+      return "You lose! Scissors beats Paper"
+    }
+  }else if(user == 'Scissors'){
+    if(computer == 'Rock'){
+      return 'You lose! Rock beats Scissors'
+    }else if(computer == 'Paper'){
+      return "You win! Scissors beats Paper"
+    }
   }
 }
-
-  function game(win){
-    let computerSelection = computerPlay()
-    let ans = round(computerSelection);
-    let nt = 0;
-    if(ans == 1){
-        nt++
-    }else if(ans == 0){
-        nt--
-    }else{
-        return "It is a tie!"
-    }   
-    return "Score: " + nt
-  }
